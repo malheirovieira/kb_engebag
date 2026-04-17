bb31f6caa6f56af97b26f46605430ab41496932c
malheirovieira <malheirovieira.g@gmail.com>
2026-04-17 17:14:52 -0300

    first commit project kb_engebag

---
+++ artigos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
