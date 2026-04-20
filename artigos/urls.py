ab09f5b7282fb4d8fb006baefa1130d17e6c7da2
malheirovieira <malheirovieira.g@gmail.com>
2026-04-19 23:55:05 -0300

    Adiciona HTML e CSS da base_conhecimento

--- artigos/urls.py
+++ artigos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, base_conhecimento
 
 urlpatterns = [
     path('', home, name='home'),
+    path('base-conhecimento/', base_conhecimento, name='base_conhecimento'),
 ]
--- artigos/views.py
+++ artigos/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def base_conhecimento(request):
+    return render(request, 'base_conhecimento.html')
